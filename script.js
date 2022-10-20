72cfdb18f4128fa657d6c3540105e9d7755c0341
Mar <90357336+margrade@users.noreply.github.com>
2022-10-19 23:46:45 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function encriptar(){
+    var text = document.getElementById('inputTexto').value.toLowerCase();
+
+    //i es para que afecte tanto a mayúsculas como minúsculas.
+    //g es para toda la línea u oración.
+    //m múltiples líneas.
+
+    var txtCifrado = text.replace(/e/igm, "enter");
+    var txtCifrado = txtCifrado.replace(/o/igm, "ober");
+    var txtCifrado = txtCifrado.replace(/i/igm, "imes");
+    var txtCifrado = txtCifrado.replace(/a/igm, "ai");
+    var txtCifrado = txtCifrado.replace(/u/igm, "ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+
+function desencriptar(){
+    var text = document.getElementById('inputTexto').value.toLowerCase();
+
+    //i es para que afecte tanto a mayúsculas como minúsculas.
+    //g es para toda la línea u oración.
+    //m múltiples líneas.
+
+    var txtCifrado = text.replace(/enter/igm, "e");
+    var txtCifrado = txtCifrado.replace(/ober/igm, "o");
+    var txtCifrado = txtCifrado.replace(/imes/igm, "i");
+    var txtCifrado = txtCifrado.replace(/ai/igm, "a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm, "u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("¡Se copió!");
+
+}
